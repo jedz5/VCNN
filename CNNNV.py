7021e6a9c152898b0c467d6d4f52ca9a37fd5526
jedz5 <jed_zh@qq.com>
2017-05-02 23:07:25 +0800

    xxx

---
+++ CNNNV.py
@@ -0,0 +1,156 @@
+'''
+A Convolutional Network implementation example using TensorFlow library.
+This example is using the MNIST database of handwritten digits
+(http://yann.lecun.com/exdb/mnist/)
+
+Author: Aymeric Damien
+Project: https://github.com/aymericdamien/TensorFlow-Examples/
+'''
+
+from __future__ import print_function
+
+import tensorflow as tf
+
+# Parameters
+learning_rate = 0.003
+training_iters = 20000
+#batch_size = 128
+display_step = 10
+
+# Network Parameters
+hexY = 11
+hexX = 17
+hexDepth = 16
+n_classes = 8
+dropout = 0.5 # Dropout, probability to keep units
+
+# tf Graph input
+x = tf.placeholder(tf.float32, [None, hexY,hexX,hexDepth])
+y = tf.placeholder(tf.float32, [None, n_classes])
+keep_prob = tf.placeholder(tf.float32) #dropout (keep probability)
+
+
+# Create some wrappers for simplicity
+def  conv2d(x, W, b, strides=1):
+    # Conv2D wrapper, with bias and relu activation
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.bias_add(x, b)
+    return tf.nn.relu(x)
+
+
+# def maxpool2d(x, k=2):
+#     # MaxPool2D wrapper
+#     return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+#                           padding='SAME')
+
+
+# Create model
+def conv_net(x, weights, biases, dropout):
+    # Reshape input picture
+    #x = tf.reshape(x, shape=[-1, 28, 28, 1])
+
+    x = batch_norm(x,bool(1),bool(1),0.999)
+    # Convolution Layer
+    conv1 = conv2d(x, weights['wc1'], biases['bc1'])
+    conv1 = batch_norm(conv1,bool(1),bool(1),0.999)
+
+    # Convolution Layer
+    conv2 = conv2d(conv1, weights['wc2'], biases['bc2'])
+    conv2 = batch_norm(conv2,bool(1),bool(1),0.999)
+
+    # Fully connected layer
+    # Reshape conv2 output to fit fully connected layer input
+    fc1 = tf.reshape(conv2, [-1, weights['wd1'].get_shape().as_list()[0]])
+    fc1 = tf.add(tf.matmul(fc1, weights['wd1']), biases['bd1'])
+    fc1 = tf.nn.relu(fc1)
+    # Apply Dropout
+    fc1 = tf.nn.dropout(fc1, dropout)
+
+    # Output, class prediction
+    out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
+    return out
+
+
+# Store layers weight & bias
+weights = {
+    # 5x5 conv, 1 input, 32 outputs
+    'wc1': tf.Variable(tf.random_normal([3, 3, hexDepth, 16])),
+    # 5x5 conv, 32 inputs, 64 outputs
+    'wc2': tf.Variable(tf.random_normal([3, 3, 16,16])),
+    # fully connected, 7*7*64 inputs, 1024 outputs
+    'wd1': tf.Variable(tf.random_normal([hexY*hexX*16, 64])),
+    # 1024 inputs, 10 outputs (class prediction)
+    'out': tf.Variable(tf.random_normal([64, n_classes]))
+}
+
+biases = {
+    'bc1': tf.Variable(tf.random_normal([16])),
+    'bc2': tf.Variable(tf.random_normal([16])),
+    'bd1': tf.Variable(tf.random_normal([64])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+def batch_norm(inputs, is_training,is_conv_out=True,decay = 0.999):
+
+    scale = tf.Variable(tf.ones([inputs.get_shape()[-1]]))
+    beta = tf.Variable(tf.zeros([inputs.get_shape()[-1]]))
+    pop_mean = tf.Variable(tf.zeros([inputs.get_shape()[-1]]), trainable=False)
+    pop_var = tf.Variable(tf.ones([inputs.get_shape()[-1]]), trainable=False)
+
+    if is_training:
+        if is_conv_out:
+            batch_mean, batch_var = tf.nn.moments(inputs,[0,1,2])
+        else:
+            batch_mean, batch_var = tf.nn.moments(inputs,[0])
+
+        train_mean = tf.assign(pop_mean,
+                               pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var,
+                              pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs,
+                batch_mean, batch_var, beta, scale, 0.001)
+    else:
+        return tf.nn.batch_normalization(inputs,
+            pop_mean, pop_var, beta, scale, 0.001)
+def train(batch_x,batch_y):
+    # Construct model
+    pred = conv_net(x, weights, biases, keep_prob)
+
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        step = 1
+        # Keep training until reach max iterations
+        while step  < 5000:
+            # Run optimization op (backprop)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
+                                           keep_prob: dropout})
+            if step % display_step == 0:
+                # Calculate batch loss and accuracy
+                loss, acc = sess.run([cost, accuracy], feed_dict={x: batch_x,
+                                                                  y: batch_y,
+                                                                  keep_prob: 1.})
+                print("Iter " + str(step) + ", Minibatch Loss= " + \
+                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.5f}".format(acc))
+            step += 1
+        print("Optimization Finished!")
+
+        # Calculate accuracy for 256 mnist test images
+        # print("Testing Accuracy:", \
+        #     sess.run(accuracy, feed_dict={x: mnist.test.images[:256],
+        #                                   y: mnist.test.labels[:256],
+        #                                   keep_prob: 1.}))
+
---
+++ loadJson.py
@@ -0,0 +1,71 @@
+import json
+import os
+import traceback
+import numpy as np
+import CNNNV as cnn
+hexY = 11
+hexX = 17
+hexDepth = 16
+def load(inFile):
+    root = None
+    with open(inFile) as jsonFile:
+        root = json.load(jsonFile)
+        plane = np.zeros((hexY,hexX,hexDepth))
+        label = np.zeros((8))
+       # for x in range(hexX):
+           # for y in range(hexY):
+        try:
+            if 'obstacles' in root:
+                for x in root['obstacles']:
+                    plane[x['y']][x['x']][0] = 0
+                    plane[x['y']][x['x']][1] = 1
+            for x in root['stacks']:
+                if x['isHuman']:
+                    plane[x['y']][x['x']][0] = 1
+                    plane[x['y']][x['x']][1] = 0
+                    if x['isWide']:
+                        plane[x['y']][x['x'] - 1][0] = 1
+                        plane[x['y']][x['x'] - 1][1] = 0
+                else:
+                    plane[x['y']][x['x']][0] = -1
+                    plane[x['y']][x['x']][1] = 0
+                plane[x['y']][x['x']][2] = x['amount']
+                plane[x['y']][x['x']][3] = x['attack']
+                plane[x['y']][x['x']][4] = x['defense']
+                plane[x['y']][x['x']][5] = x['maxDamage']
+                plane[x['y']][x['x']][6] = x['minDamage']
+                plane[x['y']][x['x']][7] = x['firstHPleft']
+                plane[x['y']][x['x']][8] = x['health']
+                plane[x['y']][x['x']][9] = x['speed']
+                plane[x['y']][x['x']][10] = x['isCanShoot']
+                plane[x['y']][x['x']][11] = x['isCanMove']
+                plane[x['y']][x['x']][12] = x['isMoved']
+                plane[x['y']][x['x']][13] = x['isRetaliate']
+                plane[x['y']][x['x']][14] = x['isWaited']
+                plane[x['y']][x['x']][15] = x['isWide']
+        except:
+            traceback.print_exc()
+        label[root['action']['actionType'] - 1] = 1.0
+    return plane,label
+
+
+if __name__ == "__main__":
+    batch = []
+    y = []
+    f_list = os.listdir(".")
+    for i in f_list:
+        if os.path.splitext(i)[1] == ".json":
+            batch.append(load(i)[0])
+            y.append(load(i)[1])
+    print("end")
+    bx = np.asarray(batch)
+    by = np.asarray(y)
+    print("end")
+    cnn.train(bx,by)
+# matrix1 = tf.constant([[3., 3.]])
+    # matrix2 = tf.constant([[2.], [2.]])
+    # product = tf.matmul(matrix1, matrix2)
+    # sess = tf.Session()
+    # result = sess.run(product)
+    # print result
+    # sess.close()
