e5a9e997da83df3828a8af37cd1148d7c51d5a94
jedz5 <jed_zh@qq.com>
2017-12-24 23:32:56 +0800

    xx

--- loadJson.py
+++ loadJson.py
@@ -2,34 +2,47 @@ import json
 import os
 import traceback
 import numpy as np
-import CNNNV as cnn
-import tensorflow as tf
 import sys
 print(sys.path)
 side = 2
 stacks = 7
-hexDepth = 7
+hexDepth = 3
+n_classes = 8
+n_spells = 4
+n_manaCost = 1
+n_all = side*stacks*hexDepth+n_spells+n_manaCost
 def load(inFile):
     root = None
     with open(inFile) as jsonFile:
         root = json.load(jsonFile)
         plane = np.zeros((side,stacks,hexDepth))
-        label = np.zeros((9,1))
-       # for x in range(hexX):
-           # for y in range(hexY):
+        label = np.zeros((n_classes,1))
+        amount = np.zeros((n_classes,1))
+        value = np.zeros((n_classes,1))
+        spells = {'26':1,'41':2,'53':3,'54':4}
         try:
+            if not 'hero' in root:
+                return
+            heroStrength = np.math.sqrt((1 + 0.05 * root['hero']['attack']) * (1 + 0.05 * root['hero']['defense']))
             for x in root['stacks']:
-                plane[x['id0']][x['slot']][0] = x['id2']
-                plane[x['id0']][x['slot']][1] = x['id3']
-
-                plane[x['id0']][x['slot']][1] = (x['id4'] == 15)
-                plane[x['id0']][x['slot']][1] = (x['id5'] == 25)
-                plane[x['id0']][x['slot']][1] = (x['id6'] == 35)
-                plane[x['id0']][x['slot']][1] = (x['id7'] == 45)
+                plane[~x['isHuman']][x['slot']][0] = x['baseAmount']
+                plane[~x['isHuman']][x['slot']][1] = x['fightValue']*heroStrength
+                plane[~x['isHuman']][x['slot']][2] = x['aiValue']*heroStrength
+                if x['isHuman']:
+                    label[x['slot']] = x['killed']
+                    amount[x['slot']] = x['baseAmount']
+                    value[x['slot']] = x['aiValue']
+            plane.resize((n_all,),refcheck=False)
+            for y in root['hero']['spells']:
+                if str(y['id']) in spells:
+                    plane[spells[str(y['id'])]+n_all-1] = 1
 
         except:
             traceback.print_exc()
-        label[root['action']['actionType'] - 1] = 1.0
+            return
+        label[7][0] = root['manaCost']
+        amount_[7][0] = root['manaCost']
+        #label[1][0] = root['win']
     return plane,label
 
 def loadData(path):
@@ -37,39 +50,36 @@ def loadData(path):
     y = []
     f_list = os.listdir(path)
     for i in f_list:
-        if os.path.splitext(i)[1] == ".json":
-            batch.append(load(i)[0])
-            y.append(load(i)[1])
+        try:
+            if os.path.splitext(i)[1] == ".json":
+                rr = load(i)
+                if rr:
+                    batch.append(rr[0])
+                    y.append(rr[1])
+                else:
+                    print("data lost")
+        except:
+            traceback.print_exc()
+            continue
     print("end")
     bx = np.asarray(batch)
     by = np.asarray(y)
     return bx,by
 
-def quick(jsonData):
-    print(jsonData)
-    matrix1 = tf.constant([[3., 3.]])
-    matrix2 = tf.constant([[2.], [2.]])
-    product = tf.matmul(matrix1, matrix2)
-    sess = tf.Session()
-    result = sess.run(product)
-    print(result)
-    sess.close()
-    return jsonData
+# def quick(jsonData):
+#     print(jsonData)
+#     matrix1 = tf.constant([[3., 3.]])
+#     matrix2 = tf.constant([[2.], [2.]])
+#     product = tf.matmul(matrix1, matrix2)
+#     sess = tf.Session()
+#     result = sess.run(product)
+#     print(result)
+#     sess.close()
+#     return jsonData
 if __name__ == "__main__":
-    # batch = []
-    # y = []
-    # f_list = os.listdir(".")
-    # # np.random.shuffle(f_list)
-    # # np.save("./result/shuffle",f_list)
-    # #f1_list =np.load("./result/shuffle.npy")
-    # for i in f_list:
-    #     if os.path.splitext(i)[1] == ".json":
-    #         batch.append(load(i)[0])
-    #         y.append(load(i)[1])
-    # bx = np.asarray(batch)
-    # by = np.asarray(y)
+    bx, by = loadData(".")
     # cnn.train(bx,by)
-    quick("hello")
+    print()
 
 
 # matrix1 = tf.constant([[3., 3.]])
--- run_main.py
+++ run_main.py
@@ -1,13 +1,27 @@
+import socket
 import json
-import os
 import traceback
-import numpy as np
-#import CNNNV as cnn
-import tensorflow as tf
-def quick(jsonData):
-    print(jsonData)
-    return jsonData
-import sys
-print(sys.path)
-if __name__ == '__main__':
-    quick("heloo")
+import socketserver
+import time
+import threading
+
+serverPort = 50007
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while 1:
+    try:
+        connectionSocket, addr = serverSocket.accept()
+        sentence = connectionSocket.recv(40960)
+        print("received ",len(sentence),sentence)
+        sentenceStr = sentence.decode('utf-8')
+        print("utf length ",len(sentenceStr))
+        root = json.loads(sentenceStr)
+        tosend = json.dumps(root).encode("utf-8");
+        connectionSocket.send(tosend)
+        connectionSocket.close()
+    except:
+        connectionSocket.close()
+        traceback.print_exc()
+        continue
