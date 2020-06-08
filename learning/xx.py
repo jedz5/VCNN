02a2ce847e3367c74d26e2a7b4b95eb761b54d8e
jedz5 <jed_zh@qq.com>
2020-06-08 16:57:16 +0800

    CNN reshape

--- learning/xx.py
+++ learning/xx.py
@@ -4,63 +4,36 @@ import numpy as np
 from torch import nn
 import torch.nn.functional as F
 
-num_FC = 11
-a = torch.ones(64,2048,device='cpu')
-b = torch.ones(num_FC,2048,1024,device='cpu')
-st = time.time()
-c = torch.matmul(a, b)
-print("cpu compute cost={}".format(time.time() - st))
-a.cuda()
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential( #input shape (1,28,28)
+            nn.Conv2d(in_channels=2, #input height
+                      out_channels=16, #n_filter
+                     kernel_size=3, #filter size
+                     stride=1, #filter step
+                     padding=1 #con2d出来的图片大小不变
+                     ), #output shape (16,28,28)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2) #2x2采样，output shape (16,14,14)
 
-a = torch.randn(64,2048,device='cpu')
-b = torch.randn(num_FC,2048,1024,device='cpu')
-st = time.time()
-a = a.cuda()
-b = b.cuda()
-st2 = time.time()
-c = torch.matmul(a, b)
-print("cuda transfor cost={}, compute cost={}".format(st2 -st,time.time() - st))
-#
-#
-l = []
-a = torch.randn(64,2048,device='cpu')
-b = [torch.randn(2048,1024,device='cpu') for i in range(num_FC)]
-str = ""
-st = time.time()
-a = a.cuda()
-# str += " trs {}".format(time.time() - st)
-for i in b:
-    st0 = time.time()
-    i = i.cuda()
-    ed0 = time.time()
-    # str += " trs {}".format(ed0 - st0)
-    c = torch.matmul(a, i)
-    # str += " cpt {}".format(time.time() - ed0)
-    l.append(c)
-# print(str)
-print("cuda compute cost={}".format(time.time() - st))
+        )
+        # self.conv2 = nn.Sequential(nn.Conv2d(16, 32, 5, 1, 2), #output shape (32,7,7)
+        #                           nn.ReLU(),
+        #                           nn.MaxPool2d(2))
+        # self.out = nn.Linear(32*7*7,10)
 
-l = []
-a = torch.ones(64,2048,device='cpu')
-b = [torch.ones(2048,1024,device='cpu') for i in range(11)]
-st = time.time()
-a = a.to(device='cuda')
-for i in b:
-    i = i.to(device='cuda')
-    c = torch.matmul(a, i)
-    l.append(c)
-print(time.time() - st)
+    def forward(self, x):
+        x = self.conv1(x)
+        # x = self.conv2(x)
+        # x = x.view(x.size(0), -1) #flat (batch_size, 32*7*7)
+        # output = self.out(x)
+        return x
 
-
-l = []
-a = torch.ones(64,2048,device='cpu')
-b = [torch.ones(2048,1024,device='cpu') for i in range(num_FC)]
-st = time.time()
-c = [torch.matmul(a, i) for i in b]
-print(time.time() - st)
-
-
-
+cnn = CNN()
+inp = [torch.arange(50,dtype=torch.float) for x in range(10)]
+inp = torch.stack(inp).reshape(-1,2,5,5)
+out = cnn(inp)
 
 
 
